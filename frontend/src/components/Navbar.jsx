eba683c4508083c2ada9439ecaa1f065e63ab04c
Anuj-jnv <anujk534208@gmail.com>
2025-08-13 13:15:51 +0530

    Added project Screenshot

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate,NavLink } from "react-router-dom";
 import { GiHamburgerMenu } from "react-icons/gi";
 import axios from "axios";
 import { toast } from "react-toastify";
